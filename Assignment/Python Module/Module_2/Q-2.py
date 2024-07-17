35cba73da74322a869828a9645bda47a07561c52
k58u <panchalkaushik89005@gmail.com>
2024-07-17 15:22:09 +0530

    update

--- Assignment/Python Module/Module_2/Q-2.py
+++ Assignment/Python Module/Module_2/Q-2.py
@@ -4,10 +4,7 @@ number = int(input("Enter a number: "))
 
 result = 1
 
-if number < 0:
-    print("Factorial is not defined for negative numbers.")
-
-else:
-    for i in range(1, number + 1):
-        result *= i
-    print(f"The factorial of ",result)
+for i in range(1, number + 1):
+    result *= i
+    
+print(f"The factorial of {number} is",result)
