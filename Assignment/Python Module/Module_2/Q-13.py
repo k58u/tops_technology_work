5534a2d1b4693c03cafcc06a3a3cb291e49fcad0
k58u <panchalkaushik89005@gmail.com>
2024-07-17 14:50:59 +0530

    updated

--- Assignment/Python Module/Module_2/Q-13.py
+++ Assignment/Python Module/Module_2/Q-13.py
@@ -1,9 +1,16 @@
-# Write a Python program to count the number of characters (character 
-# frequency) in a string 
+# # Write a Python program to count the number of characters (character 
+# # frequency) in a string 
 
 string = input("Enter a string: ")
 
-for char in frozenset(string):
-    # if char != ' ': // for count space (space ne ) 
+u_ch = ""
+empty_dictionary = dict()
 
-        print(f"Character '{char}' is {string.count(char)} times")
+for ch in string:
+    if ch not in u_ch:
+        u_ch += ch
+        empty_dictionary.update({ch:string.count(ch)})
+
+print(empty_dictionary)
+
+print(u_ch)
