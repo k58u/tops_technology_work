cc920990650ed5cbc146192622c441299f8202b5
k58u <panchalkaushik89005@gmail.com>
2025-05-30 11:44:35 +0530

    Remove Institute-Management-System folder

--- Assignment/Python Module/Module_2/Q-1.py
+++ Assignment/Python Module/Module_2/Q-1.py
@@ -1,10 +1,21 @@
 # Write a Python program to check if a number is positive, negative or zero.
 
-nums = int(input("Enter a number: "))
+# nums = int(input("Enter a number: "))
 
-if nums > 0:
-    print("The number is Positive.")
-elif nums < 0:
-    print("The number is Negative.")
+# if nums > 0:
+#     print("The number is Positive.")
+# elif nums < 0:
+#     print("The number is Negative.")
+# else:
+#     print("The number is Zero.")
+
+num = int(input("enter the number :"))
+
+if num > 0:
+    print("number is positive")
+
+elif num < 0:
+    print("number is less then negative")
+
 else:
-    print("The number is Zero.")
+    print("numbr is negative or zero")
--- Assignment/Python Module/Module_2/Q-2.py
+++ Assignment/Python Module/Module_2/Q-2.py
@@ -1,10 +1,18 @@
 # Write a Python program to get the Factorial number of given number. 
 
-number = int(input("Enter a number: "))
+# number = int(input("Enter a number: "))
 
-result = 1
+# result = 1
 
-for i in range(1, number + 1):
-    result *= i
+# for i in range(1, number + 1):
+#     result *= i
     
-print(f"The factorial of {number} is",result)
+# print(f"The factorial of {number} is",result)
+
+num = int(input("enter the number :"))
+res = 1
+
+for i in range (1 ,num + 1):
+    res *= i
+
+print(f"factorial of (num) is : ", res)
